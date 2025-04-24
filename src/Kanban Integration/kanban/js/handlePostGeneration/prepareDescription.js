7cf8464e22e29ebba7f9e5ddae3177a05407f636
Yiran Lu <yiranls@outlook.com>
2025-04-24 20:20:30 +0200

    feat: Integrate note descriptions in Kanban view
    - }Create a basic HTML template for displaying note descriptions.
    - > Append a description element to the Kanban container.
    - Populates and links the Kanban description element with content from a child note.

---
+++ src/Kanban Integration/kanban/js/handlePostGeneration/prepareDescription.js
@@ -0,0 +1,28 @@
+module.exports = async function(context) {
+    const {
+      helpers,
+    } = context;
+    
+    const { noteId: kanbanNoteId } = api.originEntity;
+    
+    const descriptionData = await api.runOnBackend(kanbanNoteId => {
+        const kanbanNote = api.getNote(kanbanNoteId);
+        const descriptionNote = kanbanNote.getChildNotes().filter(board => {
+            return board.hasLabel('kanbanDescription');
+        });
+        if (descriptionNote) {
+            return {
+                id: descriptionNote[0].noteId,
+                content: descriptionNote[0].getContent()
+            }
+        }
+    }, [kanbanNoteId]);
+    
+    
+    $('#kanban-description').html(descriptionData.content);
+    $('#kanban-description').click(() => {
+        helpers.navigateToNote(descriptionData.id);
+    });
+    $('#kanban-description').scroll(() => {
+    });
+}
